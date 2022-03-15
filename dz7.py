cfcc91701f5996a9112ea6440eb6af0ff00b2b33
Марина Невская <nev-marina@yandex.ru>
2022-03-15 21:37:32 +0300

    Changes

--- dz7.py
+++ dz7.py
@@ -1,33 +1,34 @@
 from encodings import utf_8
 import os
 
+def get_cook_book():
+    with open(r'Дз 7/recepies.txt', 'r', encoding='utf_8') as file:
+        cook_book = {}
+        for string in file:
+            dish_name = string.strip()
+            quantity = int(file.readline().strip())
+            dish = []
+            for i in range(quantity):
+                ingredient_name, quantity, measure = file.readline().strip().split('|')
+                dish.append({'ingredient_name': ingredient_name, 'quantity': quantity, 'measure': measure})
+            cook_book[dish_name] = dish
+            file.readline()
+        return cook_book
 
-import os
-with open(r'Дз 7/recepies.txt', 'r', encoding='utf_8') as file:
-    cook_book = {}
-    for string in file:
-        dish_name = string.strip()
-        quantity = int(file.readline().strip())
-        dish =[]
-        for i in range(quantity):
-            ingredient_name, quantity, measure = file.readline().strip().split('|')
-            dish.append({'ingredient_name': ingredient_name, 'quantity': quantity, 'measure': measure})
-        cook_book[dish_name] = dish
-        file.readline()
+
+# print(get_cook_book())
 
 
 def get_shop_list_by_dishes(dishes, person_count):
     shop_list = {}
     for dish in dishes:
-        for ingredient in cook_book[dish]:
-            ingredients = dict([(ingredient['ingredient_name'], {'quantity': int(ingredient['quantity']) * person_count,'measure': ingredient['measure']})])
-            if shop_list.get(ingredient['ingredient_name']) == 'None':
-                ing_list = (int(shop_list[ingredient['ingredient_name']]['quantity']) +
-                           int(shop_list[ingredient['ingredient_name']]['quantity']))
-                shop_list[ingredient['ingredient_name']]['quantity'] = ing_list
+        for ingredient in get_cook_book()[dish]:
+            ingr_name = ingredient['ingredient_name']
+            if ingr_name not in shop_list:
+                shop_list[ingr_name] = {'quantity': int(ingredient['quantity']) * person_count, 'measure': ingredient['measure']}
             else:
-                shop_list.update(ingredients)
+                shop_list[ingr_name]['quantity'] += int(ingredient['quantity']) * person_count
     return shop_list
 
 
-print(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет', 'Фахитос'], 3))
+print(get_shop_list_by_dishes(['Омлет', 'Омлет'], 2))
--- dz7_2.py
+++ dz7_2.py
@@ -1,15 +1,21 @@
 import os
 import readline
-text = []
-texts_name = os.listdir("Дз 7/Files")
 
-for name in texts_name:
-    with open(f"Дз 7/Files/{name}", encoding="utf-8") as f:
-        lines = f.readlines()
-        count_line = str(len(lines))
-        lines = [name + "\n"] + [count_line + "\n"] + lines
-        text.append(lines)
-final_text = "".join(sum((sorted(text, key=len)), []))
 
-with open("Дз 7/result_dz7.txt", "w", encoding="utf-8") as f:
-    f.write(final_text)
+def merged_file():
+    text = []
+    texts_name = os.listdir("Дз 7/Files")
+
+    for name in texts_name:
+        with open(f"Дз 7/Files/{name}", encoding="utf-8") as f:
+            lines = f.readlines()
+            count_line = str(len(lines))
+            lines_ = [name + "\n"] + [count_line + "\n"] + lines
+            text.append(lines_)
+    final_text = "".join(sum((sorted(text, key=len)), []))
+
+    with open("Дз 7/result_dz7.txt", "w", encoding="utf-8") as f:
+        f.write(final_text)
+
+
+merged_file()
